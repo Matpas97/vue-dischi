5b7fcd0f0650aebb55cd9c0ed655fa71eaaf942e
Matpas97 <passarellim22@gmail.com>
2022-09-27 17:15:39 +0200

    Push

---
+++ src/components/AlbumCardComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card-album">
+      <img class="w-100" :src="info.poster" :alt="info.title" @error="replaceWrongImageUrl" />
+      <p> {{ info.title }}</p>
+    </div>
+  </template>
+  
+  <script>
+  
+  import errorImage from '@/assets/img/logo.png'
+  
+  export default {
+      name: 'AlbumCardComponent',
+      props:{
+          info: Object
+      },
+      methods:{
+          replaceWrongImageUrl(e){
+              console.log('errore');
+            e.target.src = errorImage
+          }
+      }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  .card-album{
+      display: flex;
+      flex-direction: column;
+      padding: 10px;
+      background-color: #2e3a46;
+      color: #ccc;
+      text-align: center;
+      height: 100%;
+      p{
+          color: #fff;
+          text-transform: uppercase;
+          margin-top: 5px;
+      }
+  }
+  </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <img src="../assets/img/spotify.png" alt="Vue Dischi"/>
+
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 80px;
+    background-color: #2e3a46;
+    padding: 10px;
+    img {
+        height: 60px;;
+    }
+}
+</style>
